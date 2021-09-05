2a89e09726d93fd5cb79a30edc8949399b64081a
drehnstrom <drehnstrom@gmail.com>
2021-09-05 21:57:15 +0000

    Changes

--- main.go
+++ main.go
@@ -2,19 +2,64 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	"cloud.google.com/go/datastore"
 )
 
+var projectID string 
+
+func main() {
+
+	projectID = os.Getenv("GOOGLE_CLOUD_PROJECT")
+	if projectID == "" {
+		log.Fatal(`You need to set the environment variable "GOOGLE_CLOUD_PROJECT"`)
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+
+	}
+	log.Printf("Port set to: %s", port)
+
+	fs := http.FileServer(http.Dir("assets"))
+	mux := http.NewServeMux()
+
+	// This serves the static files in the assets folder
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	// The rest of the routes
+	mux.HandleFunc("/about", aboutHandler)
+	mux.HandleFunc("/", indexHandler)
+
+	log.Printf("Webserver listening on Port: %s", port)
+	http.ListenAndServe(":"+port, mux)
+
+}
+	
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	var pets []Pet
+	pets, error := GetPets()
+	if error != nil {
+		fmt.Print(error)
+	}
+
+	data := HomePageData{
+		PageTitle: "Pets Home Page",
+		Pets: pets,
+	}
 
 	var tpl = template.Must(template.ParseFiles("templates/index.html", "templates/layout.html"))
 
 	buf := &bytes.Buffer{}
-	err := tpl.Execute(buf, nil)
+	err := tpl.Execute(buf, data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		log.Println(err.Error())
@@ -26,11 +71,14 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	data := AboutPageData{
+		PageTitle: "About Go Pets",
+	}
 
 	var tpl = template.Must(template.ParseFiles("templates/about.html", "templates/layout.html"))
 
 	buf := &bytes.Buffer{}
-	err := tpl.Execute(buf, nil)
+	err := tpl.Execute(buf, data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		log.Println(err.Error())
@@ -40,25 +88,55 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	buf.WriteTo(w)
 	log.Println("About Page Served")
 }
-func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		
+
+
+
+// HomePageData for Index template
+type HomePageData struct {
+	PageTitle string
+	Pets []Pet
+}
+
+// AboutPageData for About template
+type AboutPageData struct {
+	PageTitle string
+}
+
+// Pet model stored in Datastore
+type Pet struct {
+	Added   time.Time `datastore:"added"`
+	Caption string    `datastore:"caption"`
+	Email   string    `datastore:"email"`
+	Image   string    `datastore:"image"`
+	Likes   int       `datastore:"likes"`
+	Owner   string    `datastore:"owner"`
+	Petname string    `datastore:"petname"`
+	Name    string    // The ID used in the datastore.
+}
+
+// GetPets Returns all pets from datastore ordered by likes in Desc Order
+func GetPets() ([]Pet, error) {	
+	var pets []Pet
+	ctx := context.Background()
+	client, err := datastore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Could not create datastore client: %v", err)
 	}
-	log.Printf("Port set to: %s", port)
 
-	fs := http.FileServer(http.Dir("assets"))
-	mux := http.NewServeMux()
+	// Create a query to fetch all Pet entities".
+	query := datastore.NewQuery("Pet").Order("-likes")
+	keys, err := client.GetAll(ctx, query, &pets)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
 
-	// This serves the static files in the assets folder 
-	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	// Set the id field on each Task from the corresponding key.
+	for i, key := range keys {
+		pets[i].Name = key.Name
+	}
 
-	// The rest of the routes
-	mux.HandleFunc("/about", aboutHandler)
-	mux.HandleFunc("/", indexHandler)
+	client.Close()
 
-	log.Printf("Webserver listening on Port: %s", port)
-	http.ListenAndServe(":"+port, mux)
-	
+	return pets, nil
 }
