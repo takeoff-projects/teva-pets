bf594dc0952b960582fb9a27227e8c24a3b83606
Tatiana <tatiana.vasylieva@takeoff.com>
2021-09-30 12:22:22 +0300

    update

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+export GOOGLE_CLOUD_PROJECT=$1
+
+docker build . --tag "gcr.io/$GOOGLE_CLOUD_PROJECT/go-pets-teva"
+docker push "gcr.io/$GOOGLE_CLOUD_PROJECT/go-pets-teva:latest"
 
 cd terraform
 terraform init
